1d97b9632ac2faaeac0000a23b2ff51ffb834353
clark-cui <cuirongchuan@163.com>
2022-05-02 23:42:08 +0800

    feature: add counter contracts

--- truffle-config.js
+++ truffle-config.js
@@ -1,41 +1,30 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const fs = require('fs');
+const api_key = fs.readFileSync('.api_key').toString().trim();
+const mnemonic = fs.readFileSync('.mnemonic').toString().trim();
 module.exports = {
-  // Uncommenting the defaults below
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks.
-  // See details at: https://trufflesuite.com/docs/truffle/reference/configuration
-  // on how to specify configuration options!
-  //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
-  //},
-  //
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows:
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-    // enabled: false,
-    // host: "127.0.0.1",
-    // adapter: {
-    //   name: "sqlite",
-    //   settings: {
-    //     directory: ".db"
-    //   }
-    // }
-  // }
+
+  compilers: {
+    solc: {
+      version: "0.8.9"
+    }
+  },
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      gas: 5500000, //gas limit
+      gasPrice: 10000000000 //10Gwei
+    },
+    goerli: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, 'https://goerli.infura.io/v3/' + api_key)
+      },
+      network_id: '5',
+      gas: 4465030,
+      gasPrice: 10000000000,
+    },
+  }
+
 };
